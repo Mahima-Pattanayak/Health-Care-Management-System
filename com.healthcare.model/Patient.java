c46248cb8505e1f61afd10366bdbff9c57f6f280
Mahima Pattanayak <164752721+Mahima-Pattanayak@users.noreply.github.com>
2024-07-14 17:56:49 +0530

    Create Patient.java

---
+++ com.healthcare.model/Patient.java
@@ -0,0 +1,19 @@
+package com.healthcare.model;
+
+public class Patient {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    // Constructor
+    public Patient(String firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters and Setters
+    // Implement as needed
+}
