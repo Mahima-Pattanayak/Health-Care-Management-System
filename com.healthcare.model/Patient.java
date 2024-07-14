5beaf18cfec2adcf54e5c3e3fa8fd233213b3f91
Mahima Pattanayak <164752721+Mahima-Pattanayak@users.noreply.github.com>
2024-07-14 18:05:23 +0530

    Update Patient.java

--- com.healthcare.model/Patient.java
+++ com.healthcare.model/Patient.java
@@ -13,4 +13,38 @@ public class Patient {
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
